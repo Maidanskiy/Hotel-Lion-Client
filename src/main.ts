d41ae35bd223d6e13e0798d505756996a4eeb6a9
Roman Maidanskiy <maidanskiy.roman@gmail.com>
2018-08-09 01:08:31 +0300

    chore: removes logger from main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,6 @@
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
-console.log(`PORT ${process.env.PORT}`) as void
-
 (async () => {
     const app = await NestFactory.create(AppModule)
     await app.listen(process.env.PORT || 3000)
