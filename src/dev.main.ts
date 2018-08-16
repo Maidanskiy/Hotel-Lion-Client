af69adf92ab5885a486cec5e4567af55fe2cf1ff
Roman Maidanskiy <maidanskiy.roman@gmail.com>
2018-08-16 23:32:40 +0300

    feat: configure CORS(2)

--- src/dev.main.ts
+++ src/dev.main.ts
@@ -6,12 +6,5 @@ dotenv.config();
 
 (async () => {
   const app = await NestFactory.create(AppModule)
-  app.enableCors({
-    origin: '*',
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    exposedHeaders: ['Authorization']
-  })
   await app.listen(process.env.PORT || 3000)
 })()
